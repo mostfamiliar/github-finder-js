9d914d750221e69ea4bdc739f7671761d784c5d9
torrence stratton <tlstratton@gmail.com>
2016-03-18 08:59:29 -0700

    get basic get request and callback working

--- js/gituser-interface.js
+++ js/gituser-interface.js
@@ -1,9 +1,14 @@
-var gitUser = require("./../js/gituser.js").gitUser;
+var getRepos = require("./../js/gituser.js").getRepos;
 
 
 $(document).ready(function(){
-  $("#userForm").click(function() {
-    gitUser();
+  var userName;
+
+  $("#formButton").click(function(event) {
+    userName = $("input#userName").val();
+    getRepos(userName);
+
+   event.preventDefault();
   });
-  event.preventDefault();
+
 });
--- js/gituser.js
+++ js/gituser.js
@@ -1,8 +1,15 @@
 apiKey = require("./../.env").apiKey;
 
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+exports.getRepos = function(user){
+  $.get('https://api.github.com/users/' + user + '/repos' + '?access_token=' + apiKey).then(function(response){
     console.log(response);
+    for(var i = 0; i < response.length; i++)
+    {
+      console.log(response[i].full_name);
+      var name = response[i].full_name
+      var description = response[i].description
+    }
+    $("#gitRepos").append("<ul><li>" + name + '<li>' + '<li>' + description + '<li>');
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
